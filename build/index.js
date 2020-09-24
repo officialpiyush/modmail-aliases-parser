80bb90ad1c515452b092fb855a5e833401f979f7
Piyush Bhangale <bhangalepiyush@gmail.com>
2020-09-24 21:00:35 +0530

    remove build dir

--- build/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Parser_1 = require("./lib/Parser");
-exports.default = Parser_1.default;
-//# sourceMappingURL=index.js.map
--- build/lib/types/ParserOptions.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-;
-//# sourceMappingURL=ParserOptions.js.map
